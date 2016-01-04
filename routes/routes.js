56ed79fe33cc8d0344bd6c8cd2894d92ad5cd41b
Mike Allanson <michael.allanson@gmail.com>
2016-01-04 16:43:19 +0000

    Move all component files to the same directory

--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,9 @@
-import About from '../components/about/About'
-import App from '../components/app/App'
-import Hindsight from '../components/hindsight/Hindsight'
-import Home from '../components/home/Home'
-import Projects from '../components/projects/Projects'
-import Shop from '../components/shop/Shop'
+import About from '../components/About'
+import App from '../components/App'
+import Hindsight from '../components/Hindsight'
+import Home from '../components/Home'
+import Projects from '../components/Projects'
+import Shop from '../components/Shop'
 
 export default {
   path: '',
