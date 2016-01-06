52a2ab3a06feaaf700262c8f9269d0dda909e227
Mike Allanson <michael.allanson@gmail.com>
2016-01-06 17:56:24 +0000

    Basic style and placeholder content for About page

--- components/About.js
+++ components/About.js
@@ -1,5 +1,19 @@
 import React from 'react'
 
 export default () => (
-  <div></div>
+  <div className='About'>
+    <img className='About-image' src='static/images/pascal.png' alt='Pascal at work' />
+    <ul className='About-statement'>
+      <li className='About-statementIcon'></li>
+      <li className='About-statementCopy'>I have clear ideas and strong values that inform a thorough process, ensuring design interacts with and facilitates business objectives.</li>
+    </ul>
+    <ul className='About-statement'>
+      <li className='About-statementIcon'></li>
+      <li className='About-statementCopy'>I have clear ideas and strong values that inform a thorough process, ensuring design interacts with and facilitates business objectives.</li>
+    </ul>
+    <ul className='About-statement'>
+      <li className='About-statementIcon'></li>
+      <li className='About-statementCopy'>I have clear ideas and strong values that inform a thorough process, ensuring design interacts with and facilitates business objectives.</li>
+    </ul>
+  </div>
 )
