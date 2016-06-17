a0db0984e01e063bc894fef5ea16df3a5626b1bd
Pascal <pascaljbarry@gmail.com>
2016-06-17 17:24:06 +0100

    lint corrections 5

--- common/components/contact/Contact.js
+++ common/components/contact/Contact.js
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default () => (
   <div className='Contact'>
-    <h1 className='Contact-text'><a className='Contact-link' href="mailto:studio@designbypascal.com">studio[at]designbypascal.com</a><br />07906 079 068<br />112 Manor Avenue<br />Brockley, London</h1>
+    <h1 className='Contact-text'><a className='Contact-link' href='mailto:studio@designbypascal.com'>studio[at]designbypascal.com</a><br />07906 079 068<br />112 Manor Avenue<br />Brockley, London</h1>
   </div>
 )
